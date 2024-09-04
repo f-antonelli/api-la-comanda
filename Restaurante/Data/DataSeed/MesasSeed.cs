c89224c311cd5b1bb6bb6b640c6d2ea176b977b8
sebast825 <sebamolina825@gmail.com>
2024-09-04 14:33:04 -0300

    data Seed

---
+++ Restaurante/Data/DataSeed/MesasSeed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Restaurante.Entities;
+using Restaurante.Entities.Enums;
+using System;
+
+namespace Restaurante.Data.Seed
+{
+    public class MesasSeed : IEntityTypeConfiguration<Mesas>
+    {
+        public void Configure(EntityTypeBuilder<Mesas> builder)
+        {
+            builder.HasData(
+                new Mesas
+                {
+                    Id = 1,
+                    Codigo = "A1B2C",
+                    Estado = EstadosMesa.ClienteEsperandoPedido
+                },
+                new Mesas
+                {
+                    Id = 2,
+                    Codigo = "D3E4F",
+                    Estado = EstadosMesa.ClienteComiendo
+                },
+                new Mesas
+                {
+                    Id = 3,
+                    Codigo = "G5H6I",
+                    Estado = EstadosMesa.ClientePagando
+                },
+                new Mesas
+                {
+                    Id = 4,
+                    Codigo = "J7K8L",
+                    Estado = EstadosMesa.Cerrada
+                }
+            );
+        }
+    }
+}
--- Restaurante/obj/Debug/net6.0/Restaurante.AssemblyInfo.cs
+++ Restaurante/obj/Debug/net6.0/Restaurante.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881465edeef957b60d2ee3588aadbe97ef7f4816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab6b319d033590ec2b4eddfd679d35bb66e637d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
